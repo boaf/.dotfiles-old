e75d4c01fe907f18df5a80a5e1743584c90874d9
Gibson Starkweather <gibson@gibson.fm>
2014-03-07 13:03:00 -0800

    we'll assume tmux exists

--- tmux/init.sh
+++ tmux/init.sh
@@ -2,7 +2,7 @@
 
 # auto-start tmux control mode if we're SSH'd
 
-if [[ $SSH_CONNECTION && hash tmux 2>/dev/null ]]; then
+if [ $SSH_CONNECTION ]; then
     WHOAMI=$(whoami)
     if tmux has-session -t $WHOAMI 2>/dev/null; then
         tmux -CC -2 attach-session -t $WHOAMI
